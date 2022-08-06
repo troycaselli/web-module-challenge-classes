f727bd2e8dbae9ca4bbb8ff523cc06ae76c164c2
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-08-06 16:11:21 -0700

    1-2 completed Person class and Car class with constructor and prototypes

--- index.js
+++ index.js
@@ -44,8 +44,23 @@ class Airplane {
 */
 
 class Person {
-  
-}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  };
+  eat(food) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(food);
+    };
+  };
+  poop() {
+    this.stomach = [];
+  };
+  toString() {
+    return `${this.name}, ${this.age}`;
+  };
+};
 
 /*
   TASK 2
@@ -62,8 +77,27 @@ class Person {
 */
 
 class Car {
-  
-}
+  constructor(model, mpg) {
+    this.model = model;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  };
+  fill(gallons) {
+    this.tank += gallons;
+  };
+  drive(distance) {
+    let travelMax = this.tank * this.milesPerGallon;
+    if(travelMax >= distance) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += travelMax;
+      this.tank = 0;
+      return `I ran out of fuel at ${travelMax} miles!`;
+    };
+  };
+};
 
 /*
   TASK 3
